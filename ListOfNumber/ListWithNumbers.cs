1727d197ee0db67d6d34c9eab4bd34f078d33078
denisov_sv@directum.ru <denisov_sv@directum.ru>
2019-01-17 16:48:27 +0400

    Добавьте файлы проекта.

---
+++ ListOfNumber/ListWithNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOfNumber
+{
+    /// <summary>
+    /// Класс для хранения коллекции чисел.
+    /// </summary>
+    public static class ListWithNumbers
+    {
+        /// <summary>
+        /// Коллекция int и decimal чисел.
+        /// </summary>
+        static readonly List<int> intNumbers = new List<int>() { 1, -2, -5, 3, 4, 0, 7, -4 };
+        static readonly List<decimal> decimalNumbers = new List<decimal> { 1, 3, 6, 78, 23, 23, 5, 8 };
+    }
+}
---
+++ ListOfNumber/Methods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOfNumber
+{
+    /// <summary>
+    /// Класс, содержащий методы для работы с коллекциями чисел
+    /// </summary>
+    public static class Methods
+    {
+        /// <summary>
+        /// Процедура сложения чисел
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public static int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Процедура вычитания чисел
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public static int Substruct(int a, int b)
+        {
+            return a - b;
+        }
+
+        private static void Sort()
+        {
+
+        }
+    }
+}
